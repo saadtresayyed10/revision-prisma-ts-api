47e8b779c6a0b7eae649d6110fddfe1f14982045
unknown <saadsyed950@gmail.com>
2025-01-31 20:24:11 +0530

    Author api completed, checking in postman

--- controller/author.controller.ts
+++ controller/author.controller.ts
@@ -11,7 +11,74 @@ export const getAllAuthor = async (req, res) => {
       },
     });
 
-    res.json({ data: authors }).status(201);
+    res.json({ data: authors }).status(200);
+  } catch (error) {
+    console.error(`Error: ${error}`);
+  }
+};
+
+//Get single Author if they have published books.
+export const getSingleAuthor = async (req, res) => {
+  try {
+    const authorId = req.params.id;
+    const author = await authorClient.findUnique({
+      where: {
+        id: authorId,
+      },
+      include: {
+        books: true,
+      },
+    });
+
+    res.json({ data: author }).status(200);
+  } catch (error) {
+    console.error(`Error: ${error}`);
+  }
+};
+
+//Add author
+export const addAuthor = async (req, res) => {
+  try {
+    const authorData = req.body;
+    const author = await authorClient.create({
+      data: authorData,
+    });
+
+    res.json({ data: author }).status(201);
+  } catch (error) {
+    console.error(`Error: ${error}`);
+  }
+};
+
+//Update author
+export const updateAuthor = async (req, res) => {
+  try {
+    const authorId = req.params.id;
+    const authorData = req.body;
+    const author = await authorClient.update({
+      where: {
+        id: authorId,
+      },
+      data: authorData,
+    });
+
+    res.json({ data: author }).status(200);
+  } catch (error) {
+    console.error(`Error: ${error}`);
+  }
+};
+
+//Delete author
+export const deleteAuthor = async (req, res) => {
+  try {
+    const authorId = req.params.id;
+    const author = await authorClient.delete({
+      where: {
+        id: authorId,
+      },
+    });
+
+    res.json({ message: "Author deleted successfully" }).status(200);
   } catch (error) {
     console.error(`Error: ${error}`);
   }
--- routes/author.routes.ts
+++ routes/author.routes.ts
@@ -1,8 +1,18 @@
 import { Router } from "express";
-import { getAllAuthor } from "../controller/author.controller";
+import {
+  getAllAuthor,
+  addAuthor,
+  deleteAuthor,
+  getSingleAuthor,
+  updateAuthor,
+} from "../controller/author.controller";
 
 const authorRoutes = Router();
 
 authorRoutes.get("/", getAllAuthor);
+authorRoutes.get("/:id", getSingleAuthor);
+authorRoutes.get("/", addAuthor);
+authorRoutes.get("/:id", updateAuthor);
+authorRoutes.get("/:id", deleteAuthor);
 
 export default authorRoutes;
